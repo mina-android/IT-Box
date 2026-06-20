f63f9948d3603eec11701275436f7e9a3f5edf9e
Mina Ashraf <104459927+mina-android@users.noreply.github.com>
2026-06-20 20:44:36 +0300

    Release 1.0.1

---
+++ android/app/src/main/java/com/example/inventorya/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.inventorya;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/ma/itbox/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ma.itbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
